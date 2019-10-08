978b47fa38bcdb83fdc1e6b1312ab3d54d2368e0
rizul <rizulgoyal23@gmail.com>
2019-10-07 20:30:32 -0400

    limit

--- day1_swift/main.swift
+++ day1_swift/main.swift
@@ -8,5 +8,48 @@
 
 import Foundation
 
-print("Hello, World!")
+var str = "Hello, playground"
+
+var s: String
+
+print(str)
+var a:Int=100
+//a=300
+print(a)
+
+
+var c=20
+var b=30
+var d=c+b
+var sum="SUM of \(c) and \(b) is \(d)"
+print("Values: \(c),\(b) Sum: \(d)")
+print(sum)
+var p="😂😡"
+print(p)
+var 🐻="Lion"
+print(🐻)
+var x1 = (1,"Pritesh","Patel")
+print(x1)
+print(x1.1)
+print(x1.2)
+print(x1.0)
+var x2 = (collegeID: 1,collegeName :"Lambton College",cityName: "Toronto")
+print(x2.collegeID)
+print(x2.cityName)
+print(x2.collegeName)
+
+var x3 = (CollegeName: x2.collegeName, CityName: x2.cityName)
+print(x3)
+var (_,_,CollegeName) = x2
+print(CollegeName)
+var _ = 10000
+var g = 1...10
+print(g)
+for i in stride(from: 50, to: 0, by: -5){
+    print("Rizul \(i)")
+}
+for i in 1...10{
+    print("5 * \(i) = \(5*i)")
+}
+
 
